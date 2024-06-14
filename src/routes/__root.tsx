acf3cd81992e88c712cbc3893b71e404d4bc873b
adefrol <adefrolx@gmail.com>
2024-06-15 02:38:33 +0800

    devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
     component: () => (
@@ -9,7 +8,6 @@ export const Route = createRootRoute({
             <Outlet />
             <Sonner />
             <Toaster />
-            <TanStackRouterDevtools />
         </>
     ),
 });
